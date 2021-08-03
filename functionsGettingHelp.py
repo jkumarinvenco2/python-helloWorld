5a357afe307f916dce9979a3477690685bccea55
JK <Jitender.Kumar@invenco.com>
2021-08-03 18:16:00 +1200

    functions

---
+++ functionsGettingHelp.py
@@ -0,0 +1,9 @@
+def least_difference(a,b,c):
+    diff1= abs(a-b)
+    diff2= abs(b-c)
+    diff3= abs(a-c)
+    return min(diff1, diff2, diff3)
+print(least_difference(1,10,100))
+print(least_difference(1212,123,35435))
+
+
