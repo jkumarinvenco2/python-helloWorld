d61a841f757853d32d231204490eda1b4c60a642
JK <Jitender.Kumar@invenco.com>
2021-08-03 18:02:26 +1200

    with if statement

--- python-helloWorld.py
+++ python-helloWorld.py
@@ -1,2 +1,13 @@
-print("Hello world")
-print("hello JKumar here")
+spam_amount = 0
+print(spam_amount)
+# Ordering Spam, egg, Spam, Spam, bacon and Spam (4 more servings of Spam)
+spam_amount = spam_amount + 4
+print(spam_amount)
+if spam_amount <= 0:
+    pass
+else:
+    print("But I don't want any spam")
+viking_song = "Spam" * spam_amount
+print(viking_song)
+
+
